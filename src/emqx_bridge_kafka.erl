97c758f2985e0a39ac65febc79ae968d44cf4554
崔征泽 <cui.zhengze@huidatech.cn>
2022-07-01 13:50:35 +0800

    add get

--- src/emqx_bridge_kafka.erl
+++ src/emqx_bridge_kafka.erl
@@ -106,7 +106,7 @@ on_client_connected(ClientInfo = #{clientid := ClientId}, ConnInfo, _Env) ->
             {client_id,ClientId},
             {ts,M * 1000000 + S},
             {cluster_node,node()},
-            {conn_info,ConnInfo.peerhost}
+            {conn_info,ConnInfo:get(peerhost)}
             % {client_info,ClientInfo}
     ]),
     % ekaf:produce_async(<<"linkstatus">>, Json).
