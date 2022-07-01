15e481471f687c16cce77588bcc58580e915303d
崔征泽 <cui.zhengze@huidatech.cn>
2022-07-01 14:14:13 +0800

    add tuple to list

--- src/emqx_bridge_kafka.erl
+++ src/emqx_bridge_kafka.erl
@@ -106,7 +106,7 @@ on_client_connected(ClientInfo = #{clientid := ClientId}, ConnInfo, _Env) ->
             {client_id,ClientId},
             {ts,M * 1000000 + S},
             {cluster_node,node()},
-            {ip,maps:get(peername,ConnInfo)}
+            {ip,tuple_to_list(maps:get(peername,ClientInfo))}
             % {client_info,ClientInfo}
     ]),
     % ekaf:produce_async(<<"linkstatus">>, Json).
