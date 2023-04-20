26eceaeb91c5ee24bc135300a69b699d49cca529
huidatech <48268801+huidatech@users.noreply.github.com>
2023-04-20 18:36:59 +0800

    Update emqx_bridge_kafka.erl
    modify

--- src/emqx_bridge_kafka.erl
+++ src/emqx_bridge_kafka.erl
@@ -264,7 +264,7 @@ on_message_publish(Message, _Env) ->
                     brod:produce_sync(brod_client_1, ProduceTopic, PartitionFun, <<>>, Json);
                 false ->
                     brod:produce_sync(brod_client_1, <<"linkbytes">>, PartitionFun, <<>>, Payload);
-            end,
+            end;
     end,
     % ekaf:produce_async(ProduceTopic, Json),
     % ekaf:produce_async(Topic, Payload),
